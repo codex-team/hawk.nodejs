07f72fbc43f81f48e16457bbccc6a4eec94ff3ba
N0str <ntpcp@yandex.ru>
2018-03-25 18:35:57 +0300

    Fix eslint

--- example/simple/promiseExample.js
+++ example/simple/promiseExample.js
@@ -1,19 +1,19 @@
 'use strict';
 
 var hawkCatcher = require('@codexteam/hawk.nodejs')({
-    accessToken: '69d86244-f792-47ad-8e9a-23fee358e062',
-    url: 'http://localhost:3000/catcher/nodejs'
+  accessToken: '69d86244-f792-47ad-8e9a-23fee358e062',
+  url: 'http://localhost:3000/catcher/nodejs'
 });
 
 try {
-    throw new Exception('');
+  throw new Exception('');
 } catch (e) {
-    hawkCatcher.catchExceptionPromise(e, {comment: 'Exception in general module'})
-        .then(function () {
-            console.log('Exception successfully sent to Hawk');
-        })
-        .catch(function (err) {
-            console.log('Error occured: ', err);
-        });
+  hawkCatcher.catchExceptionPromise(e, {comment: 'Exception in general module'})
+    .then(function () {
+      console.log('Exception successfully sent to Hawk');
+    })
+    .catch(function (err) {
+      console.log('Error occured: ', err);
+    });
 }
 
--- src/hawk.js
+++ src/hawk.js
@@ -8,95 +8,95 @@ let Promise = require('es6-promise').Promise;
  *
  */
 let hawkCatcher = (function () {
-    /**
-    * URL          – API endpoint
-    * AccessToken  – Token for project in hawk profile
-    * @type {string}
-    */
-    let url = 'https://hawk.so/catcher/nodejs',
-            accessToken = null;
+  /**
+   * URL          – API endpoint
+   * AccessToken  – Token for project in hawk profile
+   * @type {string}
+   */
+  let url = 'https://hawk.so/catcher/nodejs',
+      accessToken = null;
 
-    /**
-    * Initialize Hawk Catcher with config
-    * @param {url: String, accessToken: String} — configuration parameters
-    */
-    let init = function (config) {
-        accessToken = config.accessToken;
-        url = config.url || url;
+  /**
+   * Initialize Hawk Catcher with config
+   * @param {url: String, accessToken: String} — configuration parameters
+   */
+  let init = function (config) {
+    accessToken = config.accessToken;
+    url = config.url || url;
+  };
+
+  /**
+   * Convert error object to the format for Hawk catcher API
+   *
+   * @param error – Error object
+   * @param custom {comment: String} – custom parameters
+   *
+   * @returns Object – hashmap with data prepared for the API endpoint
+   */
+  let prepare = function (error, custom={}) {
+    let data = {
+      token: accessToken,
+      message: error.name + ': ' + error.message,
+      type: error.name,
+      tag: 'fatal',
+      stack: error.stack,
+      time: new Date().toISOString(),
+
+      // custom params
+      comment: custom.comment || ''
     };
 
-    /**
-    * Convert error object to the format for Hawk catcher API
-    *
-    * @param error – Error object
-    * @param custom {comment: String} – custom parameters
-    *
-    * @returns Object – hashmap with data prepared for the API endpoint
-    */
-    let prepare = function (error, custom={}) {
-        let data = {
-            token: accessToken,
-            message: error.name + ': ' + error.message,
-            type: error.name,
-            tag: 'fatal',
-            stack: error.stack,
-            time: new Date().toISOString(),
+    return data;
+  };
 
-            // custom params
-            comment: custom.comment || ''
-        };
+  /**
+   *Prepare error data for sending and send the to the Hawk Catcher API
+   *
+   * @param errorText – Node.js Error object
+   * @param custom {comment: String} – custom parameters
+   */
+  let catchExceptionCallback = function (errorText, custom, callback) {
+    request.post({
+      url: url,
+      form: prepare(errorText, custom)
+    }, callback);
+  };
 
-        return data;
-    };
+  /**
+   * Prepare error data for sending and send the to the Hawk Catcher API
+   *
+   * @param errorText – Node.js Error object
+   * @param custom {comment: String} – custom parameters
+   *
+   * @returns Promise
+   */
+  let catchExceptionPromise = function (errorText, custom) {
+    return new Promise(function (resolve, reject) {
+      request.post({
+        url: url,
+        form: prepare(errorText, custom)
+      }, function (error, response, body) {
+        try {
+          if (response.statusCode != 200) {
+            console.log('[HawkCatcher] Got status from Backend: ', response.statusCode);
+            reject();
+          } else {
+            console.log('[HawkCatcher] Received: ', body);
+            resolve();
+          }
+        } catch (err) {
+          console.log('[HawkCatcher] Exception: ', err);
+          reject();
+        }
+      });
+    });
+  };
 
-    /**
-    * Prepare error data for sending and send the to the Hawk Catcher API
-    *
-    * @param errorText – Node.js Error object
-    * @param custom {comment: String} – custom parameters
-    */
-    let catchExceptionCallback = function (errorText, custom, callback) {
-        request.post({
-            url: url,
-            form: prepare(errorText, custom)
-        }, callback);
-    };
-
-    /**
-    * Prepare error data for sending and send the to the Hawk Catcher API
-    *
-    * @param errorText – Node.js Error object
-    * @param custom {comment: String} – custom parameters
-    *
-    * @returns Promise
-    */
-    let catchExceptionPromise = function (errorText, custom) {
-        return new Promise(function (resolve, reject) {
-            request.post({
-                url: url,
-                form: prepare(errorText, custom)
-            }, function (error, response, body) {
-                try {
-                    if (response.statusCode != 200) {
-                        console.log('[HawkCatcher] Got status from Backend: ', response.statusCode);
-                        reject();
-                    } else {
-                        console.log('[HawkCatcher] Received: ', body);
-                        resolve();
-                    }
-                } catch (err) {
-                    console.log('[HawkCatcher] Exception: ', err);
-                    reject();
-                }
-            });
-        });
-    };
-
-    return {
-        init,
-        catchExceptionCallback,
-        catchExceptionPromise
-    };
+  return {
+    init,
+    catchExceptionCallback,
+    catchExceptionPromise
+  };
 })();
 
 /**
@@ -105,11 +105,11 @@ let hawkCatcher = (function () {
  * @param config – optional dictionary with {url: String, accessToken: String}
  */
 module.exports = function (config) {
-    if (typeof config.accessToken !== 'undefined') {
-        hawkCatcher.init(config);
-
-        return hawkCatcher;
-    }
+  if (typeof config.accessToken !== 'undefined') {
+    hawkCatcher.init(config);
 
     return hawkCatcher;
+  }
+
+  return hawkCatcher;
 };
