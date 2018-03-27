d1ba61525f4507304f19034a435284353f202530
N0str <ntpcp@yandex.ru>
2018-03-28 00:03:09 +0300

    Fix doc

--- example/globalExample.js
+++ example/globalExample.js
@@ -6,7 +6,7 @@ var hawkCatcher = require('@codexteam/hawk.nodejs')({
   accessToken: '69d86244-f792-47ad-8e9a-23fee358e062'
 });
 
-// or you can simply call with or without optional callback as argument
+// init global Exception Catcher
 hawkCatcher.initGlobalCatcher();
 
 // Try to execute undefined method to illustrate an Exception
